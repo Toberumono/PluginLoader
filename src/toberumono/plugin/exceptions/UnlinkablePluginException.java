dd8b9651974e8dbd3a0d829aec32578b53bab1f9
Toberumono <JoshuaLipstone@gmail.com>
2015-12-30 13:03:20 -0600

    Added UnlinkablePluginException.

---
+++ src/toberumono/plugin/exceptions/UnlinkablePluginException.java
@@ -0,0 +1,60 @@
+package toberumono.plugin.exceptions;
+
+import toberumono.plugin.manager.ManageablePlugin;
+
+/**
+ * Thrown when attempting an operation that requires a {@link ManageablePlugin plugin} to be linkable on a
+ * {@link ManageablePlugin plugin} that is not linkable.
+ *
+ * @author Toberumono
+ */
+public class UnlinkablePluginException extends PluginException {
+	
+	/**
+	 * Constructs a new exception with {@code null} as its detail message. The cause is not initialized, and may subsequently
+	 * be initialized by a call to {@link #initCause}.
+	 */
+	public UnlinkablePluginException() {
+		super();
+	}
+	
+	/**
+	 * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be
+	 * initialized by a call to {@link #initCause}.
+	 *
+	 * @param message
+	 *            the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+	 */
+	public UnlinkablePluginException(String message) {
+		super(message);
+	}
+	
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * <p>
+	 * Note that the detail message associated with {@code cause} is <em>not</em> automatically incorporated in this
+	 * exception's detail message.
+	 *
+	 * @param message
+	 *            the detail message (which is saved for later retrieval by the {@link #getMessage()} method).
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is
+	 *            permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public UnlinkablePluginException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	/**
+	 * Constructs a new exception with the specified cause and a detail message of
+	 * {@code (cause==null ? null : cause.toString())} (which typically contains the class and detail message of
+	 * {@code cause}).
+	 *
+	 * @param cause
+	 *            the cause (which is saved for later retrieval by the {@link #getCause()} method). (A {@code null} value is
+	 *            permitted, and indicates that the cause is nonexistent or unknown.)
+	 */
+	public UnlinkablePluginException(Throwable cause) {
+		super(cause);
+	}
+}
